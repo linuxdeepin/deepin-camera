e2660046d6eafab65ac33676658c24658b0bca57
hujianbo <hujianbo@uniontech.com>
2020-07-31 15:54:50 +0800

    fix: 修改bug40657
    Description:
    1、缩略图删除后，如果没有后续文件加入，需要清掉多选
    2、拍照结束文案修改了，对应翻译跟上
    Log:
    Bug:
    Task:
    Issue: fix #1, fix #2

--- src/translations/deepin-camera_zh_CN.ts
+++ src/translations/deepin-camera_zh_CN.ts
@@ -4,17 +4,14 @@
 <context>
     <name>CMainWindow</name>
     <message>
-        <location filename="../src/mainwindow.cpp" line="379"/>
         <source>Video recording is in progress. Close the window?</source>
         <translation>正在录制视频，是否关闭？</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="414"/>
         <source>Open folder</source>
         <translation>打开文件夹</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="609"/>
         <source>Settings</source>
         <translation>设置</translation>
     </message>
@@ -22,13 +19,11 @@
 <context>
     <name>CloseDialog</name>
     <message>
-        <location filename="../src/closedialog.h" line="39"/>
         <source>Cancel</source>
         <translatorcomment>取消</translatorcomment>
         <translation>取消</translation>
     </message>
     <message>
-        <location filename="../src/closedialog.h" line="39"/>
         <source>Close</source>
         <translatorcomment>关闭</translatorcomment>
         <translation>关闭</translation>
@@ -41,17 +36,14 @@
 <context>
     <name>ImageItem</name>
     <message>
-        <location filename="../src/imageitem.cpp" line="94"/>
         <source>Copy</source>
         <translation>复制</translation>
     </message>
     <message>
-        <location filename="../src/imageitem.cpp" line="97"/>
         <source>Delete</source>
         <translation>删除</translation>
     </message>
     <message>
-        <location filename="../src/imageitem.cpp" line="99"/>
         <source>Open folder</source>
         <translation>打开文件夹</translation>
     </message>
@@ -59,109 +51,87 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../src/mainwindow.cpp" line="179"/>
         <source>You don&apos;t have permission to operate this folder</source>
         <translation>无权限打开文件夹</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="181"/>
         <source>Close</source>
         <translation>关闭</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="197"/>
         <source>Invalid folder</source>
         <translation>无效文件夹</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="218"/>
         <source>Open folder</source>
         <translation>打开文件夹</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="345"/>
         <source>File not saved</source>
         <translation>文件未保存</translation>
     </message>
     <message>
-        <location filename="../src/settings_translation.cpp" line="5"/>
         <source>Settings</source>
         <translation>设置</translation>
     </message>
     <message>
-        <location filename="../src/settings_translation.cpp" line="6"/>
         <source>Basic</source>
         <translation>基本设置</translation>
     </message>
     <message>
-        <location filename="../src/settings_translation.cpp" line="7"/>
         <source>File location</source>
         <translation>保存路径</translation>
     </message>
     <message>
-        <location filename="../src/settings_translation.cpp" line="8"/>
         <source>Save to</source>
         <translation>存储位置</translation>
     </message>
     <message>
-        <location filename="../src/settings_translation.cpp" line="9"/>
         <source>Shoot</source>
         <translation>拍照设置</translation>
     </message>
     <message>
-        <location filename="../src/settings_translation.cpp" line="10"/>
         <source>Burst mode</source>
         <translation>连拍</translation>
     </message>
     <message>
-        <location filename="../src/settings_translation.cpp" line="11"/>
         <source>Self-timer delay</source>
         <translation>拍照延时设置</translation>
     </message>
     <message>
-        <location filename="../src/settings_translation.cpp" line="12"/>
         <source>Output</source>
         <translation>输出设置</translation>
     </message>
     <message>
-        <location filename="../src/settings_translation.cpp" line="13"/>
         <source>Output format</source>
         <translation>输出格式</translation>
     </message>
     <message>
-        <location filename="../src/settings_translation.cpp" line="14"/>
         <source>Photo format</source>
         <translation>照片格式</translation>
     </message>
     <message>
-        <location filename="../src/settings_translation.cpp" line="15"/>
         <source>Video format</source>
         <translation>视频格式</translation>
     </message>
     <message>
-        <location filename="../src/settings_translation.cpp" line="16"/>
         <source>4 pcs</source>
         <translation>4张</translation>
     </message>
     <message>
-        <location filename="../src/settings_translation.cpp" line="17"/>
         <source>10 pcs</source>
         <translation>10张</translation>
     </message>
     <message>
-        <location filename="../src/settings_translation.cpp" line="18"/>
         <source>None</source>
         <translation>无</translation>
     </message>
     <message>
-        <location filename="../src/main.cpp" line="57"/>
-        <location filename="../src/main.cpp" line="62"/>
         <source>Camera</source>
         <translatorcomment>相机</translatorcomment>
         <translation>相机</translation>
     </message>
     <message>
-        <location filename="../src/main.cpp" line="63"/>
         <source>Camera is an image and video capture utility using your PC camera or webcam.</source>
         <translatorcomment>相机是一款简单易用的摄像头应用工具，可实现拍照、录制视频等功能。</translatorcomment>
         <translation>相机是一款简单易用的摄像头应用工具，可实现拍照、录制视频等功能。</translation>
@@ -170,24 +140,18 @@
 <context>
     <name>ThumbnailsBar</name>
     <message>
-        <location filename="../src/thumbnailsbar.cpp" line="91"/>
-        <location filename="../src/thumbnailsbar.cpp" line="106"/>
-        <location filename="../src/thumbnailsbar.cpp" line="182"/>
-        <location filename="../src/thumbnailsbar.cpp" line="377"/>
         <source>Take photo</source>
         <translation>拍照</translation>
     </message>
     <message>
-        <location filename="../src/thumbnailsbar.cpp" line="190"/>
         <source>Stop Taking photos</source>
-        <translation type="unfinished"></translation>
+        <translation>结束拍照</translation>
     </message>
     <message>
         <source>Stop Taking photo</source>
         <translation type="vanished">结束拍照</translation>
     </message>
     <message>
-        <location filename="../src/thumbnailsbar.cpp" line="389"/>
         <source>Record video</source>
         <translation>录制视频</translation>
     </message>
@@ -199,17 +163,14 @@
         <translation type="vanished">结束拍照</translation>
     </message>
     <message>
-        <location filename="../src/videowidget.cpp" line="103"/>
         <source>Stop taking video</source>
         <translation>结束录制</translation>
     </message>
     <message>
-        <location filename="../src/videowidget.cpp" line="216"/>
         <source>No webcam found</source>
         <translation>未连接摄像头</translation>
     </message>
     <message>
-        <location filename="../src/videowidget.cpp" line="245"/>
         <source>The webcam is in use</source>
         <translation>摄像头已被占用</translation>
     </message>
