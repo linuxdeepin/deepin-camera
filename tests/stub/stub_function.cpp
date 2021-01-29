51d427ed983cca3dc82f5242306434c7b868fd00
zhangchenga <zhangchenga@uniontech.com>
2021-01-29 10:55:20 +0800

    feat: videowidget打桩提升单元测试覆盖率
    Description: videowidget打桩提升单元测试覆盖率
    
    Log: 无
    Change-Id: I068176992e40d572c319c42145cc8019859e2d7d

--- tests/stub/stub_function.cpp
+++ tests/stub/stub_function.cpp
@@ -14,6 +14,10 @@ Stub_Function::Stub_Function()
 v4l2_dev_t *Stub_Function::get_v4l2_device_handler()
 {
     v4l2_dev_t *v4l2_devlist = (v4l2_dev_t *)malloc(sizeof(v4l2_dev_t));
+    v4l2_devlist->list_stream_formats = (v4l2_stream_formats_t *)malloc(sizeof(v4l2_stream_formats_t));
+    v4l2_devlist->list_stream_formats[0].list_stream_cap = (v4l2_stream_cap_t *)malloc(sizeof(v4l2_stream_cap_t));
+    v4l2_devlist->list_stream_formats[0].list_stream_cap[0].width = 640;
+    v4l2_devlist->list_stream_formats[0].list_stream_cap[0].width = 480;
     return v4l2_devlist;
 }
 
@@ -58,6 +62,110 @@ qint64 Stub_Function::msecsTo(const QDateTime &)
 {
     return 0;
 }
+
+int Stub_Function::get_wayland_status()
+{
+    return true;
+}
+
+int Stub_Function::camInit_OK(const char *devicename)
+{
+    return E_OK;
+}
+
+int Stub_Function::camInit_FORMAT_ERR(const char *devicename)
+{
+    return E_FORMAT_ERR;
+}
+
+int Stub_Function::camInit_NO_DEVICE_ERR(const char *devicename)
+{
+    return E_NO_DEVICE_ERR;
+}
+
+bool Stub_Function::isActive()
+{
+    return true;
+}
+
+bool Stub_Function::isVisible_Countdown()
+{
+    return true;
+}
+
+bool Stub_Function::isVisible_Label()
+{
+    return true;
+}
+
+Stub_Function::DeviceStatus Stub_Function::getdevStatus_CAM_CANUSE()
+{
+    return CAM_CANUSE;
+}
+
+double Stub_Function::get_video_time_capture_hour_5()
+{
+    return 5;
+}
+
+double Stub_Function::get_video_time_capture_hour_60()
+{
+    return 60;
+}
+
+double Stub_Function::get_video_time_capture_hour_21966()
+{
+    return 21966;
+}
+
+double Stub_Function::get_video_time_capture_hour_40271()
+{
+    return 40271;
+}
+
+int Stub_Function::video_capture_get_save_video()
+{
+    return 1;
+}
+
+v4l2_device_list_t *Stub_Function::get_device_list_1()
+{
+    v4l2_device_list_t *v4l2_device_list = (v4l2_device_list_t *)malloc(sizeof(v4l2_device_list_t));
+    v4l2_device_list->list_devices = new v4l2_dev_sys_data_t[1];
+    v4l2_device_list->num_devices = 1;
+    QString str1 = "/dev/video0";
+    v4l2_device_list->list_devices[0].device = (char *)malloc(strlen(str1.toLatin1().data()) + 1);
+    strcpy(v4l2_device_list->list_devices[0].device, str1.toLatin1().data());
+    return v4l2_device_list;
+}
+
+v4l2_device_list_t *Stub_Function::get_device_list_2()
+{
+    v4l2_device_list_t *v4l2_device_list = (v4l2_device_list_t *)malloc(sizeof(v4l2_device_list_t));
+    v4l2_device_list->list_devices = new v4l2_dev_sys_data_t[2];
+    v4l2_device_list->num_devices = 2;
+    QString str1 = "/dev/video0";
+    QString str2 = "/dev/video2";
+    v4l2_device_list->list_devices[0].device = (char *)malloc(strlen(str1.toLatin1().data()) + 1);
+    strcpy(v4l2_device_list->list_devices[0].device, str1.toLatin1().data());
+    v4l2_device_list->list_devices[1].device = (char *)malloc(strlen(str2.toLatin1().data()) + 1);
+    strcpy(v4l2_device_list->list_devices[1].device, str2.toLatin1().data());
+    return v4l2_device_list;
+
+}
+
+int Stub_Function::start_encoder_thread()
+{
+    return 0;
+}
+
+void Stub_Function::start()
+{
+
+}
+
+
+
 
 
 
--- tests/stub/stub_function.h
+++ tests/stub/stub_function.h
@@ -28,6 +28,37 @@ public:
     enum DeviceStatus getdevStatus();
     qint64 msecsTo(const QDateTime &);
 
+    //videowidget
+    int get_wayland_status();//获取wayland状态函数打桩
+    //调用delayInit分支
+    int camInit_OK(const char *devicename);//返回值为E_OK(进入该分支会出现错误)
+    int camInit_FORMAT_ERR(const char *devicename);//返回值为E_FORMAT_ERR
+    int camInit_NO_DEVICE_ERR(const char *devicename);//返回值为E_NO_DEVICE_ERR
+    //调用showCountdown分支
+    bool isActive();//定时器使用中函数打桩
+    bool isVisible_Countdown();//倒计时可见函数打桩
+    bool isVisible_Label();//闪光灯Label可见函数打桩
+    enum DeviceStatus getdevStatus_CAM_CANUSE();//获取相机可用状态函数打桩
+    //调用showRecTime
+    //获取写video的时间m_nCount打桩函数
+    double get_video_time_capture_hour_5();//0时0分5秒,小于3为不正常时长
+    double get_video_time_capture_hour_60();//0时1分0秒,调用秒数为0分支
+    double get_video_time_capture_hour_21966();//6时6分6秒
+    double get_video_time_capture_hour_40271();//11时11分11秒
+    //调用onEndBtnClicked
+    int video_capture_get_save_video();
+    //获取设备列表，进入设备数目为1分支
+    v4l2_device_list_t *get_device_list_1();
+    //获取设备列表，进入设备数目为2分支
+    v4l2_device_list_t *get_device_list_2();
+    //开启线程打桩
+    int start_encoder_thread();
+    //MajorImageProcessingThread开始函数打桩
+    void start();
+
+
+
+
 };
 
 #endif // STUB_FUNCTION_H
