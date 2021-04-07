0a2a7d14b2420f97fecab23da0f55a3f6b6116fd
shicetu <shicetu@uniontech.com>
2021-04-07 13:10:34 +0800

    fix: jpg图片编码错误
    424除以16含有小数，因此会造成数据偏移。在调整分辨率中，屏蔽掉宽度非8，16整数倍的分辨率
    
    Log: jpg图片编码错误
    Change-Id: I09b9e28f10d4b60990d9bdafc6bace97ac87068e

--- src/src/Settings.cpp
+++ src/src/Settings.cpp
@@ -118,6 +118,7 @@ void Settings::setNewResolutionList()
                         (list_stream_formats[format_index].list_stream_cap[i].width < 7680
                          && list_stream_formats[format_index].list_stream_cap[i].height < 4320) &&
                         ((list_stream_formats[format_index].list_stream_cap[i].width % 8) == 0
+                         && (list_stream_formats[format_index].list_stream_cap[i].width % 16) == 0
                          && (list_stream_formats[format_index].list_stream_cap[i].height % 8) ==  0)) {
                     //加入分辨率的字符串
                     QString res_str = QString("%1x%2").arg(list_stream_formats[format_index].list_stream_cap[i].width).arg(list_stream_formats[format_index].list_stream_cap[i].height);
