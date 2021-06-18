2730d5f35595a17b3a26430a860b08fe843d370b
wuzhigang <wuzhigang@uniontech.com>
2021-06-18 14:43:43 +0800

    fix: 调整测试用例结构，对测试用例进行归类，解决一些测试用例之间的依赖。
    Log: no
    Change-Id: Id8f168f2f725b1b555dcc50cdf127489095e41ff

--- tests/test-prj-running.sh
+++ tests/test-prj-running.sh
@@ -1,23 +1,39 @@
-rm -rf ~/Pictures/相机/*.jpg
-rm -rf ~/Pictures/Camera/*.jpg
-cp -r /data/source/deepin-camera/jpegtest/*.jpg ~/Pictures/相机/
-cp -r /data/source/deepin-camera/jpegtest/*.jpg ~/Pictures/Camera/
-rm -rf ~/Videos/相机/*.webm
-rm -rf ~/Videos/Camera/*.webm
-cp -r /data/source/deepin-camera/webmtest/*.webm ~/Videos/相机/
-cp -r /data/source/deepin-camera/webmtest/*.webm ~/Videos/Camera/
-rm -rf ../build-ut
-mkdir ../build-ut
-cd ../build-ut
-cmake -DCMAKE_BUILD_TYPE=Debug ..
-make -j16
-#mv asan.log* asan_${PROJECT_REALNAME}.log
+remove_cache(){
+    rm -rf ~/Pictures/相机/*.jpg
+    rm -rf ~/Pictures/Camera/*.jpg
+    rm -rf ~/Videos/相机/*.webm
+    rm -rf ~/Videos/Camera/*.webm
+    rm -rf ../build-ut
+    rm -rf ~/Videos/Camera
+    rm -rf ~/Pictures/Camera
+}
 
-workdir=$(cd ../$(dirname $0)/build-ut; pwd)
-PROJECT_NAME=deepin-camera-test #可执行程序的文件名
-PROJECT_REALNAME=deepin-camera  #项目名称
-mkdir -p html
-mkdir -p report
+make_new_dir(){
+     mkdir ../build-ut
+     mkdir -p ../build-ut/html
+     mkdir -p ../build-ut/report
+     mkdir ~/Videos/Camera/
+     mkdir ~/Pictures/Camera/
+}
+
+copy_test_files() {
+     cp -r /data/source/deepin-camera/jpegtest/*.jpg ~/Pictures/相机/
+     cp -r /data/source/deepin-camera/jpegtest/*.jpg ~/Pictures/Camera/
+     cp -r /data/source/deepin-camera/webmtest/*.webm ~/Videos/相机/
+     cp -r /data/source/deepin-camera/webmtest/*.webm ~/Videos/Camera/
+}
+
+build_exe(){
+     cd ../build-ut
+     cmake -DCMAKE_BUILD_TYPE=Debug ..
+     make -j16
+}
+
+create_coverage_report(){
+     workdir=$(cd ../$(dirname $0)/build-ut; pwd)
+     PROJECT_NAME=deepin-camera-test #可执行程序的文件名
+     PROJECT_REALNAME=deepin-camera  #项目名称
+
      echo " ===================CREAT LCOV REPROT==================== "
      lcov --directory ./tests/CMakeFiles/${PROJECT_REALNAME}.dir --zerocounters
      ./tests/${PROJECT_NAME} --gtest_output=xml:./report/report_deepin-camera.xml
@@ -30,12 +46,13 @@ mkdir -p report
      echo " -- Coverage files have been output to ${CMAKE_BINARY_DIR}/html"
      mv ./html/index.html ./html/cov_${PROJECT_REALNAME}.html
      mv asan.log* asan_${PROJECT_REALNAME}.log
-#    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+}
 
-#rm -rf ~/Pictures/相机/*.jpg
-#rm -rf ~/Pictures/Camera/*.jpg
 
-#rm -rf ~/Videos/相机/*.webm
-#rm -rf ~/Videos/Camera/*.webm
+remove_cache
+make_new_dir
+copy_test_files
+build_exe
+create_coverage_report
 exit 0
 
