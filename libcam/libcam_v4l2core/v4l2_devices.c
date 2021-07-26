b42671a74acb4fa276853f4604a35e159b64faf6
ut003654 <tanlang@uniontech.com>
2021-07-26 21:20:57 +0800

    fix: 限定设备数最多为2个
    限定设备数最多为2个
    
    Log: 限定设备数最多为2个
    Change-Id: I3d4eabc6e8da2c7d83cd29b4ed67bcdb9711dc2f

--- libcam/libcam_v4l2core/v4l2_devices.c
+++ libcam/libcam_v4l2core/v4l2_devices.c
@@ -170,8 +170,14 @@ int enum_v4l2_devices()
 //             list_entry; \
 //             list_entry = udev_list_entry_get_next(list_entry))
     //udev_list_entry_foreach(dev_list_entry, devices)
+    int count = 0;
     for (dev_list_entry = devices;dev_list_entry;dev_list_entry = getUdev()->m_udev_list_entry_get_next(dev_list_entry))
     {
+        if (count >= 2)//max read two devices
+        {
+            break;
+        }
+        
         const char *path;
 
         /*
@@ -289,11 +295,12 @@ int enum_v4l2_devices()
         my_device_list.list_devices[num_dev-1].devnum = strtoull(getUdev()->m_udev_device_get_sysattr_value(dev, "devnum"), NULL, 10);
 
         getUdev()->m_udev_device_unref(dev);
+        ++count;
     }
     /* Free the enumerator object */
     getUdev()->m_udev_enumerate_unref(enumerate);
 
-    my_device_list.num_devices = num_dev;
+    my_device_list.num_devices = count - 1;
 
     return(E_OK);
 }
