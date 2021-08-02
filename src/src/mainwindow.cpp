be64261732aa2710d2b9e6e57af9a1dff2cdf01d
xiepengfei <xiepengfei@uniontech.com>
2021-08-02 15:32:50 +0800

    fix: 修复偶现退出全屏
    添加窗口事件过滤
    
    Log: 修复部分已知问题
    Change-Id: Ibf7ce93269f3009f914e1bd8a72ada93ade1f5f9

--- src/src/mainwindow.cpp
+++ src/src/mainwindow.cpp
@@ -1928,6 +1928,11 @@ void CMainWindow::changeEvent(QEvent *event)
 {
     Q_UNUSED(event);
 
+    if (!(windowState() & Qt:: WindowFullScreen) && event->type() == QEvent::WindowStateChange) {
+        qInfo() << "want to out window fullscreen!" << windowState();
+        showFullScreen();
+        return;
+    }
     if (windowState() == Qt::WindowMinimized || (windowState() == (Qt::WindowMinimized | Qt::WindowMaximized))) {
         if (m_thumbnail->m_nStatus == STATPicIng)
             m_thumbnail->findChild<DPushButton *>(BUTTON_PICTURE_VIDEO)->click();
