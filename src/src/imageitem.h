173c1d0d7104e8fdd16b20d376bb8f1b4bf63426
wuzhigang <wuzhigang@uniontech.com>
2021-08-16 10:09:58 +0800

    feat: 删除未使用的已屏蔽代码
    Description: 删除未使用的已屏蔽代码
    
    Log: no
    Change-Id: I1658af15b8d80bd1d34a8d175e21e76f6511726e

--- src/src/imageitem.h
+++ src/src/imageitem.h
@@ -29,20 +29,11 @@
 #include <QFileInfo>
 #include <QDateTime>
 #include <QPropertyAnimation>
-//#ifdef private
-//#undef private
-//#include <dprintpreviewwidget.h>
-//#include <dprintpreviewdialog.h>
-//#define private public
-//#else
 
 #include <dprintpreviewwidget.h>
 #include <dprintpreviewdialog.h>
-//#endif
 DWIDGET_USE_NAMESPACE
 
-//#define THUMBNAIL_WIDTH 50//缩略图图元宽度
-//#define THUMBNAIL_HEIGHT 50//缩略图图元高度
 #define THUMBNAIL_PIXMAP_SIZE 50//缩略图图像缩放前尺寸,考虑了内存占用和清晰度两方面情况的取值
 #define SELECTED_WIDTH 58//选中的缩略图图元宽高
 
