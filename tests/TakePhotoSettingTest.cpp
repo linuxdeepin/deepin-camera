384239bfba5e991ed5611d4abcd5880b171d79a7
fengli <fengli@uniontech.com>
2021-11-03 17:09:06 +0800

    feat: 添加新增功能单元测试
    添加新增功能单元测试
    
    Log: no
    Change-Id: I67089393630c4f6df28544436ffcc465ebe44aeb

--- tests/TakePhotoSettingTest.cpp
+++ tests/TakePhotoSettingTest.cpp
@@ -203,6 +203,57 @@ TEST_F(TakePhotoSettingTest, delayfoldBtnClicked)
     if (nullptr != m_takePhotoSet)
         m_takePhotoSet->delayfoldBtnClicked();
 }
+
+TEST_F(TakePhotoSettingTest, exposureBtnClicked)
+{
+    if (nullptr != m_takePhotoSet) {
+        circlePushButton *exposureBtn = m_takePhotoSet->findChild<circlePushButton *>(EXPOSURE_BTN);
+//        m_takePhotoSet->unfoldBtnClicked();
+//        QTest::qWait(300);
+        Q_ASSERT(exposureBtn->isVisible());
+        m_takePhotoSet->exposureBtnClicked(true);
+        QTest::qWait(500);
+        ExposureSlider *slider = m_takePhotoSet->findChild<ExposureSlider *>(EXPOSURE_SLIDER);
+        if (!slider)
+            return;
+        Q_ASSERT(slider->isVisible());
+        QTest::qWait(200);
+        m_takePhotoSet->exposureBtnClicked(true);
+    }
+}
+
+TEST_F(TakePhotoSettingTest, exposureSlider)
+{
+    if (nullptr != m_takePhotoSet) {
+        circlePushButton *exposureBtn = m_takePhotoSet->findChild<circlePushButton *>(EXPOSURE_BTN);
+        Q_ASSERT(exposureBtn->isVisible());
+        m_takePhotoSet->exposureBtnClicked(true);
+        QTest::qWait(500);
+        ExposureSlider *slider = m_takePhotoSet->findChild<ExposureSlider *>(EXPOSURE_SLIDER);
+        if (!slider)
+            return;
+        Q_ASSERT(slider->isVisible());
+
+        QWheelEvent wheelEvent(slider->rect().topLeft() + QPoint(10, 20), 40, Qt::MiddleButton, Qt::NoModifier);
+        QApplication::sendEvent(slider, &wheelEvent);
+
+        QTest::qWait(500);
+        QKeyEvent downEvent(QEvent::KeyPress, Qt::Key_Down, Qt::NoModifier);
+        QApplication::sendEvent(slider, &downEvent);
+
+        QTest::qWait(500);
+        QKeyEvent upEvent(QEvent::KeyPress, Qt::Key_Up, Qt::NoModifier);
+        QApplication::sendEvent(slider, &upEvent);
+
+        QTest::qWait(500);
+        QKeyEvent enterEvent(QEvent::KeyPress, Qt::Key_Return, Qt::NoModifier);
+        QApplication::sendEvent(slider, &enterEvent);
+
+        QTest::qWait(200);
+        m_takePhotoSet->exposureBtnClicked(true);
+    }
+}
+
 /**
  *  @brief takePhotoSettingAreaWidget折叠设置
  */
