1fef23379fa83faec718abfe4e0a496815675d59
xiepengfei <xiepengfei@uniontech.com>
2022-01-11 14:18:50 +0800

    feat: 添加QCamera实现摄像头读取数据
    添加QCamera实现摄像头读取数据
    
    Task: https://ams.uniontech.com/task-view-4849.html
    Log: QCamera实现摄像头读取数据
    Change-Id: I1da929954ffe67b7777d41afc655e01d6429b627

---
+++ src/src/camera.cpp
@@ -0,0 +1,96 @@
+/*
+* Copyright (C) 2020 ~ 2021 Uniontech Software Technology Co.,Ltd.
+*
+* Author:     hujianbo <hujianbo@uniontech.com>
+*             shicetu <shicetu@uniontech.com>
+* Maintainer: hujianbo <hujianbo@uniontech.com>
+*             shicetu <shicetu@uniontech.com>
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#define TMPPATH "/tmp/tmp.jpg"
+
+#include "camera.h"
+
+#include <QUrl>
+#include <QtMultimediaWidgets/QCameraViewfinder>
+
+Camera::Camera()
+{
+    m_cameraInfoList = QCameraInfo::availableCameras();
+    initMember();
+}
+
+void Camera::initMember()
+{
+    currentCamera = 0;
+    m_camera = new QCamera(QCameraInfo::defaultCamera());
+    m_imageCapture = new QCameraImageCapture(m_camera);
+    m_mediaRecoder = new QMediaRecorder(m_camera);
+
+    m_camera->setCaptureMode(QCamera::CaptureStillImage);
+
+    connect(m_imageCapture, &QCameraImageCapture::imageCaptured, this, &Camera::imageCapture);
+
+    m_camera->start();
+
+    QCameraViewfinderSettings viewfinderSettings;
+    viewfinderSettings.setResolution(640, 480);
+    m_camera->setViewfinderSettings(viewfinderSettings);
+}
+
+void Camera::switchCamera()
+{
+    currentCamera++;
+    if (currentCamera > m_cameraInfoList.size())
+        currentCamera = 0;
+    m_camera->stop();
+    m_camera = new QCamera(m_cameraInfoList.at(currentCamera));
+}
+
+QList<QSize> Camera::getSupportResolutions()
+{
+    if (!m_camera)
+        return QList<QSize>();
+
+    QList<QSize> supportResolutionList = m_camera->supportedViewfinderResolutions();
+    if (supportResolutionList.isEmpty())
+        qInfo() << "Support Resolution is Empty!";
+
+    return supportResolutionList;
+}
+
+void Camera::setCameraResolution(QSize &size)
+{
+    if (m_camera) {
+        QCameraViewfinderSettings set = m_camera->viewfinderSettings();
+        set.setResolution(size);
+        m_camera->setViewfinderSettings(set);
+    }
+}
+
+void Camera::captureImage()
+{
+    if (!m_camera)
+        return;
+
+    if (m_imageCapture->isReadyForCapture()) {
+        m_imageCapture->capture("/tmp/123.jpg");
+    }
+}
+
+QCamera *Camera::getCamera()
+{
+    return m_camera;
+}
+
---
+++ src/src/camera.h
@@ -0,0 +1,94 @@
+/*
+* Copyright (C) 2020 ~ 2021 Uniontech Software Technology Co.,Ltd.
+*
+* Author:     hujianbo <hujianbo@uniontech.com>
+*             shicetu <shicetu@uniontech.com>
+* Maintainer: hujianbo <hujianbo@uniontech.com>
+*             shicetu <shicetu@uniontech.com>
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "videosurface.h"
+
+#include <QObject>
+#include <QCamera>
+#include <QList>
+#include <QCameraInfo>
+#include <QMediaRecorder>
+#include <QCameraImageCapture>
+
+class Camera : public QObject
+        {
+    Q_OBJECT
+public:
+    explicit Camera();
+    /**
+     * @brief initMember
+     * 初始化参数
+     */
+    void initMember();
+    /**
+     * @brief switchCamera 切换摄像头
+     */
+    void switchCamera();
+
+    /**
+     * @brief getSupportResolutions 获取支持的分辨率列表
+     * @return 当前摄像头分辨率列表
+     */
+    QList<QSize> getSupportResolutions();
+    /**
+     * @brief setCameraResolution
+     * 设置当前摄像头的分辨率
+     */
+    void setCameraResolution(QSize&);
+
+    /**
+     * @brief stopCamera 停止当前相机
+     */
+    void stopCamera();
+
+    /**
+     * @brief startCamera 开始当前相机
+     */
+    void startCamera();
+
+    /**
+     * @brief getCamera 提供直接操作摄像头接口
+     * @return QCamera指针
+     */
+    QCamera* getCamera();
+
+public slots:
+    /**
+     * @brief captureImage 抓取当前图像
+     */
+    void captureImage();
+
+signals:
+    void imageCapture(int, QImage);
+
+private:
+    QCamera                 *m_camera;
+    QMediaRecorder          *m_mediaRecoder;
+    QCameraImageCapture     *m_imageCapture;
+
+    QList<QCameraInfo>      m_cameraInfoList;      ///摄像头信息列表
+    int currentCamera;                             ///当前的摄像头
+};
+
+#endif //CAMERA_H
