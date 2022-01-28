319c3c77d33ae737f985cef9feecc226c98c8c84
houchengqiu <houchengqiu@uniontech.com>
2022-01-28 17:02:30 +0800

    fix: 解决sw启动相机，界面卡死的问题
    Description: 解决sw启动相机，界面卡死的问题
    
    Log: 解决sw启动相机，界面卡死的问题
    
    Change-Id: Ie8a91f35d890f0794f66c763d9ed9a1bc5ccc221

--- src/main.cpp
+++ src/main.cpp
@@ -122,7 +122,7 @@ int main(int argc, char *argv[])
     CApplication a(argc, argv);
 
     qApp->setObjectName("deepin-camera");
-#if defined (__mips__) || defined(_loongarch) || defined(__loongarch__) || defined(__loongarch64)
+#if !defined (__mips__) && !defined(_loongarch) && !defined(__loongarch__) && !defined(__loongarch64)
     qApp->setAttribute(Qt::AA_UseHighDpiPixmaps);
     // overwrite DApplication default value
     qApp->setAttribute(Qt::AA_ForceRasterWidgets, false);
