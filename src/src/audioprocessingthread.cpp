0f6d18d42316dd9a366ac6f3b6d6766877d00584
starhcq <40752904+starhcq@users.noreply.github.com>
2022-04-11 09:58:09 +0800

    fix: 解决GStreamer音频采集内存泄露问题 (#128)
    * fix: 解决GStreamer音频采集内存泄露问题
    
    Description: 解决GStreamer音频采集内存泄露问题
    
    Log: 解决GStreamer音频采集内存泄露问题
    
    * fix: 解决GStreamer音频采集内存泄露问题
    
    Description: 解决GStreamer音频采集内存泄露问题
    
    Log: 解决GStreamer音频采集内存泄露问题
    
    Co-authored-by: houchengqiu <houchengqiu@uniontech.com>

--- src/src/audioprocessingthread.cpp
+++ src/src/audioprocessingthread.cpp
@@ -104,11 +104,13 @@ void AudioProcessingThread::run()
 
     audio_stop(audio_ctx);
     audio_delete_buffer(m_auidoBuffer);
+    delete [] m_data;
 
     return;
 }
 
 AudioProcessingThread::~AudioProcessingThread()
 {
+    delete [] m_data;
     qDebug() << "~AudioProcessingThread";
 }
