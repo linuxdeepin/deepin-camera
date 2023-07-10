96df75a89eb31dcd51179405ad36eefae1f8ffa6
xiepengfei <xiepengfei@uniontech.com>
2023-07-10 17:27:52 +0800

    fix: 修复wayland下录像未停止问题
    通过kwayland接口获取wayland下窗口信息，当全屏覆盖相机时停止录像
    
    Bug: https://pms.uniontech.com/bug-view-102292.html
    Log: 修复部分已知问题
    
    	修改：     debian/control
    	修改：     src/CMakeLists.txt
    	修改：     src/src/mainwindow.cpp
    	修改：     src/src/windowstatethread.cpp
    	修改：     src/src/windowstatethread.h

--- src/src/windowstatethread.cpp
+++ src/src/windowstatethread.cpp
@@ -9,10 +9,32 @@
 #include <DWindowManagerHelper>
 #include <QMutexLocker>
 
-windowStateThread::windowStateThread(QObject *parent /*= nullptr*/)
+windowStateThread::windowStateThread(bool isWayland, QObject *parent /*= nullptr*/)
     : QThread (parent)
 {
     setObjectName("windowStateThread");
+#ifdef KF5_WAYLAND_FLAGE_ON
+    if (isWayland) {
+        qInfo() << __FUNCTION__ << __LINE__  << "KF5_WAYLAND_FLAGE_ON is open!!";
+        //wayland自动识别窗口
+        m_connectionThread = new QThread(this);
+        m_connectionThreadObject = new ConnectionThread();
+        connect(m_connectionThreadObject, &ConnectionThread::connected, this,
+        [this] {
+            m_eventQueue = new EventQueue(this);
+            m_eventQueue->setup(m_connectionThreadObject);
+
+            Registry *registry = new Registry(this);
+            qDebug() << "开始安装注册wayland服务...";
+            setupRegistry(registry);
+        },
+        Qt::QueuedConnection
+               );
+        m_connectionThreadObject->moveToThread(m_connectionThread);
+        m_connectionThread->start();
+        m_connectionThreadObject->initConnection();
+    }
+#endif
 }
 
 void windowStateThread::run()
@@ -69,4 +91,81 @@ QList<DForeignWindow *> windowStateThread::workspaceWindows() const
     }
     return windowList;
 }
+
+#ifdef KF5_WAYLAND_FLAGE_ON
+void windowStateThread::setupRegistry(Registry *registry)
+{
+    connect(registry, &Registry::compositorAnnounced, this,
+    [this, registry](quint32 name, quint32 version) {
+        qDebug() << "开始创建wayland合成器...";
+        m_compositor = registry->createCompositor(name, version, this);
+        qDebug() << "wayland合成器已创建";
+    }
+           );
+
+    connect(registry, &Registry::clientManagementAnnounced, this,
+    [this, registry](quint32 name, quint32 version) {
+        qDebug() << "开始创建wayland客户端管理...";
+        m_clientManagement = registry->createClientManagement(name, version, this);
+        qDebug() << "wayland客户端管理已创建";
+//        qDebug() << QDateTime::currentDateTime().toString(QLatin1String("hh:mm:ss.zzz ")) << "createClientManagement";
+        connect(m_clientManagement, &ClientManagement::windowStatesChanged, this,
+        [this] {
+            m_windowStates = m_clientManagement->getWindowStates();
+            qDebug() << "当前窗口的数量1(ClientManagement::windowStatesChanged):" << m_windowStates.count();
+            qDebug() << "Get new window states";
+            this->waylandwindowinfo(m_windowStates);
+        }
+               );
+    }
+           );
+
+    connect(registry, &Registry::interfacesAnnounced, this,
+    [this] {
+        Q_ASSERT(m_compositor);
+        Q_ASSERT(m_clientManagement);
+        qDebug() << "request getWindowStates";
+        m_windowStates = m_clientManagement->getWindowStates();
+        qDebug() << "当前窗口的数量2(Registry::interfacesAnnounced):" << m_windowStates.count();
+        this->waylandwindowinfo(m_windowStates);
+    }
+           );
+
+    qDebug() << "设置wayland注册的事件队列...";
+    registry->setEventQueue(m_eventQueue);
+    qDebug() << "设置wayland注册的链接线程...";
+    registry->create(m_connectionThreadObject);
+    qDebug() << "开始执行wayland注册...";
+    registry->setup();
+    qDebug() << "wayland注册完成";
+}
+
+void windowStateThread::waylandwindowinfo(const QVector<ClientManagement::WindowState> &windowStates)
+{
+    if (windowStates.count() == 0) {
+        return;
+    }
+
+    bool aheandInCamera = false;
+    QRect rect;
+    for (int i = 0; i < windowStates.count(); ++i) {
+        if (QString(windowStates[i].resourceName) != "deepin-camera") {
+            if (!aheandInCamera)
+                continue;
+            else {
+                if (windowStates[i].isFullScreen) {
+                    QRect fullscreenRect(windowStates[i].geometry.x, windowStates[i].geometry.y,
+                                         windowStates[i].geometry.width, windowStates[i].geometry.height);
+                    if (fullscreenRect.contains(rect))
+                        emit someWindowFullScreen();
+                }
+            }
+        } else {
+            aheandInCamera = true;
+            rect = QRect(windowStates[i].geometry.x, windowStates[i].geometry.y,
+                         windowStates[i].geometry.width, windowStates[i].geometry.height);
+        }
+    }
+}
+#endif
 
--- src/src/windowstatethread.h
+++ src/src/windowstatethread.h
@@ -13,12 +13,20 @@
 #include <QThread>
 #include <QMutex>
 
+#ifdef KF5_WAYLAND_FLAGE_ON
+#include <KF5/KWayland/Client/connection_thread.h>
+#include <KF5/KWayland/Client/clientmanagement.h>
+#include <KF5/KWayland/Client/event_queue.h>
+#include <KF5/KWayland/Client/registry.h>
+using namespace KWayland::Client;
+#endif
+
 class windowStateThread : public QThread
 {
     Q_OBJECT
 
 public:
-    explicit windowStateThread(QObject *parent = nullptr);
+    explicit windowStateThread(bool isWayland, QObject *parent = nullptr);
     QList<DForeignWindow *> workspaceWindows() const;
     void run();
 
@@ -26,6 +34,35 @@ signals:
     void someWindowFullScreen();
 
 private:
+#ifdef KF5_WAYLAND_FLAGE_ON
+    /**
+     * @brief wayland获取屏幕窗口信息的安装注册函数
+     * @param registry
+     */
+    void setupRegistry(Registry *registry);
+
+    /**
+     * @brief wayland获取屏幕窗口信息
+     * @param m_windowStates
+     */
+    void waylandwindowinfo(const QVector<ClientManagement::WindowState> &m_windowStates);
+#endif
+
+private:
+#ifdef KF5_WAYLAND_FLAGE_ON
+    // 获取wayland窗口信息相关。 wayland获取窗口的方法对于x11有很大的区别
+    QThread *m_connectionThread;
+    EventQueue *m_eventQueue = nullptr;
+    ConnectionThread *m_connectionThreadObject;
+    Compositor *m_compositor = nullptr;
+    PlasmaWindowManagement *m_windowManagement = nullptr;
+    ClientManagement *m_clientManagement = nullptr;
+    QVector<ClientManagement::WindowState> m_windowStates;
+    /**
+      * @brief mips平台创建缓存文件的路径
+      */
+    std::string m_tempPath;
+#endif
 
 };
 
