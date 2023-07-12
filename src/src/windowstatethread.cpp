414886226dacb40e226d2322e2c336528bcdb6cf
pengfeixx <63588879+pengfeixx@users.noreply.github.com>
2023-07-12 13:03:21 +0800

    fix: 修复wayland下录像未停止问题 (#253)
    通过kwayland接口获取wayland下窗口信息，当全屏覆盖相机时停止录像
    
    Bug: https://pms.uniontech.com/bug-view-102292.html
    Log: 修复部分已知问题

--- src/src/windowstatethread.cpp
+++ src/src/windowstatethread.cpp
@@ -9,10 +9,40 @@
 #include <DWindowManagerHelper>
 #include <QMutexLocker>
 
-windowStateThread::windowStateThread(QObject *parent /*= nullptr*/)
+windowStateThread::windowStateThread(bool isWayland, QObject *parent /*= nullptr*/)
     : QThread (parent)
 {
     setObjectName("windowStateThread");
+#ifdef KF5_WAYLAND_FLAGE_ON
+    if (isWayland) {
+        qInfo() << __FUNCTION__ << __LINE__  << "KF5_WAYLAND_FLAGE_ON is open!!";
+        //wayland自动识别窗口
+        m_connectionThread = new QThread(this);
+        m_connectionThreadObject = new ConnectionThread();
+        connect(m_connectionThreadObject, &ConnectionThread::connected, this,
+        [this] {
+            m_eventQueue = new EventQueue(this);
+            m_eventQueue->setup(m_connectionThreadObject);
+
+            Registry *registry = new Registry(this);
+            setupRegistry(registry);
+        },
+        Qt::QueuedConnection
+               );
+        m_connectionThreadObject->moveToThread(m_connectionThread);
+        m_connectionThread->start();
+        m_connectionThreadObject->initConnection();
+    }
+#endif
+}
+
+windowStateThread::~windowStateThread()
+{
+#ifdef KF5_WAYLAND_FLAGE_ON
+    m_connectionThread->quit();
+    m_connectionThread->wait();
+    m_connectionThreadObject->deleteLater();
+#endif
 }
 
 void windowStateThread::run()
@@ -69,4 +99,68 @@ QList<DForeignWindow *> windowStateThread::workspaceWindows() const
     }
     return windowList;
 }
+
+#ifdef KF5_WAYLAND_FLAGE_ON
+void windowStateThread::setupRegistry(Registry *registry)
+{
+    connect(registry, &Registry::compositorAnnounced, this,
+    [this, registry](quint32 name, quint32 version) {
+        m_compositor = registry->createCompositor(name, version, this);
+    }
+           );
+
+    connect(registry, &Registry::clientManagementAnnounced, this,
+    [this, registry](quint32 name, quint32 version) {
+        m_clientManagement = registry->createClientManagement(name, version, this);
+        connect(m_clientManagement, &ClientManagement::windowStatesChanged, this,
+        [this] {
+            m_windowStates = m_clientManagement->getWindowStates();
+            this->waylandwindowinfo(m_windowStates);
+        }
+               );
+    }
+           );
+
+    connect(registry, &Registry::interfacesAnnounced, this,
+    [this] {
+        Q_ASSERT(m_compositor);
+        Q_ASSERT(m_clientManagement);
+        m_windowStates = m_clientManagement->getWindowStates();
+        this->waylandwindowinfo(m_windowStates);
+    }
+           );
+
+    registry->setEventQueue(m_eventQueue);
+    registry->create(m_connectionThreadObject);
+    registry->setup();
+}
+
+void windowStateThread::waylandwindowinfo(const QVector<ClientManagement::WindowState> &windowStates)
+{
+    if (windowStates.count() == 0) {
+        return;
+    }
+
+    bool aheandInCamera = false;
+    QRect rect;
+    for (int i = 0; i < windowStates.count(); ++i) {
+        if (QString(windowStates[i].resourceName) != "deepin-camera") {
+            if (!aheandInCamera)
+                continue;
+            else {
+                if (windowStates[i].isFullScreen) {
+                    QRect fullscreenRect(windowStates[i].geometry.x, windowStates[i].geometry.y,
+                                         windowStates[i].geometry.width, windowStates[i].geometry.height);
+                    if (fullscreenRect.contains(rect))
+                        emit someWindowFullScreen();
+                }
+            }
+        } else {
+            aheandInCamera = true;
+            rect = QRect(windowStates[i].geometry.x, windowStates[i].geometry.y,
+                         windowStates[i].geometry.width, windowStates[i].geometry.height);
+        }
+    }
+}
+#endif
 
