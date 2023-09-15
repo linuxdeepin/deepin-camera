0bf02cd7762bcfdfa20ad117cd3c31ab699bd1fc
xiepengfei <xiepengfei@uniontech.com>
2023-09-15 14:57:30 +0800

    fix: 同步release/eagle代码至master
    同步wayland下全屏窗口覆盖停止录像，并添加dwayland适配
    
    Log: 修复部分已知问题

--- src/src/windowstatethread.h
+++ src/src/windowstatethread.h
@@ -13,12 +13,29 @@
 #include <QThread>
 #include <QMutex>
 
+#ifdef USE_DEEPIN_WAYLAND
+#ifdef DWAYLAND
+#include <DWayland/Client/clientmanagement.h>
+#include <DWayland/Client/registry.h>
+#include <DWayland/Client/connection_thread.h>
+#include <DWayland/Client/event_queue.h>
+#else
+#include <KF5/KWayland/Client/connection_thread.h>
+#include <KF5/KWayland/Client/clientmanagement.h>
+#include <KF5/KWayland/Client/event_queue.h>
+#include <KF5/KWayland/Client/registry.h>
+#endif
+
+using namespace KWayland::Client;
+#endif // USE_DEEPIN_WAYLAND
+
 class windowStateThread : public QThread
 {
     Q_OBJECT
 
 public:
-    explicit windowStateThread(QObject *parent = nullptr);
+    explicit windowStateThread(bool isWayland, QObject *parent = nullptr);
+    ~windowStateThread();
     QList<DForeignWindow *> workspaceWindows() const;
     void run();
 
@@ -26,6 +43,35 @@ signals:
     void someWindowFullScreen();
 
 private:
+#ifdef USE_DEEPIN_WAYLAND
+    /**
+     * @brief wayland获取屏幕窗口信息的安装注册函数
+     * @param registry
+     */
+    void setupRegistry(Registry *registry);
+
+    /**
+     * @brief wayland获取屏幕窗口信息
+     * @param m_windowStates
+     */
+    void waylandwindowinfo(const QVector<ClientManagement::WindowState> &m_windowStates);
+#endif
+
+private:
+#ifdef USE_DEEPIN_WAYLAND
+    // 获取wayland窗口信息相关。 wayland获取窗口的方法对于x11有很大的区别
+    QThread *m_connectionThread;
+    EventQueue *m_eventQueue = nullptr;
+    ConnectionThread *m_connectionThreadObject;
+    Compositor *m_compositor = nullptr;
+    PlasmaWindowManagement *m_windowManagement = nullptr;
+    ClientManagement *m_clientManagement = nullptr;
+    QVector<ClientManagement::WindowState> m_windowStates;
+    /**
+      * @brief mips平台创建缓存文件的路径
+      */
+    std::string m_tempPath;
+#endif
 
 };
 
