43704bfd7253c7c0917a4c7782a6c63eecccfc49
fengli <fengli@uniontech.com>
2023-12-08 15:00:00 +0800

    fix: modify the default video format for recording to webm
    For some hardware, modify the default video format for recording to webm
    
    Log: For some hardware, modify the default video format for recording to
    webm
    
    Bug: https://pms.uniontech.com/bug-view-227831.html

---
+++ src/src/globalutils.h
@@ -0,0 +1,17 @@
+// Copyright (C) 2020 ~ 2021 Uniontech Software Technology Co.,Ltd.
+// SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef GLOBALUTILS_H
+#define GLOBALUTILS_H
+
+#include <QString>
+
+class GlobalUtils
+{
+public:
+    static bool isBXCBoard();
+};
+
+#endif // GLOBALUTILS_H
