6762697bf911b9b675f281e93dedec7e7554fb43
Liu Zheng <liuzheng@uniontech.com>
2024-11-07 10:07:03 +0800

    fix: Remove full screen occlusion and stop recording function
    Remove full screen occlusion and stop recording function
    
    Log：Remove full screen occlusion and stop recording function
    Task: https://pms.uniontech.com/story-view-37517.html

--- src/src/windowstatethread.cpp
+++ src/src/windowstatethread.cpp
@@ -13,37 +13,11 @@ windowStateThread::windowStateThread(bool isWayland, QObject *parent /*= nullptr
     : QThread (parent)
 {
     setObjectName("windowStateThread");
-#ifdef USE_DEEPIN_WAYLAND
-    if (isWayland) {
-        qInfo() << __FUNCTION__ << __LINE__  << "KF5_WAYLAND_FLAGE_ON is open!!";
-        //wayland自动识别窗口
-        m_connectionThread = new QThread(this);
-        m_connectionThreadObject = new ConnectionThread();
-        connect(m_connectionThreadObject, &ConnectionThread::connected, this,
-        [this] {
-            m_eventQueue = new EventQueue(this);
-            m_eventQueue->setup(m_connectionThreadObject);
-
-            Registry *registry = new Registry(this);
-            setupRegistry(registry);
-        },
-        Qt::QueuedConnection
-               );
-        m_connectionThreadObject->moveToThread(m_connectionThread);
-        m_connectionThread->start();
-        m_connectionThreadObject->initConnection();
-    }
-#endif
 }
 
 windowStateThread::~windowStateThread()
 {
-#ifdef USE_DEEPIN_WAYLAND
-    if (m_connectionThread != nullptr) {
-        m_connectionThread->terminate();
-        m_connectionThread->wait();
-    }
-#endif
+
 }
 
 void windowStateThread::run()
@@ -55,7 +29,8 @@ void windowStateThread::run()
 
         foreach(DForeignWindow *window, list) {
             if (window && window->windowState() == Qt::WindowState::WindowFullScreen) {
-                emit someWindowFullScreen();
+                //去掉全屏的窗口信号处理
+                //emit someWindowFullScreen();
             }
         }
         //线程休眠1秒
@@ -101,67 +76,4 @@ QList<DForeignWindow *> windowStateThread::workspaceWindows() const
     return windowList;
 }
 
-#ifdef USE_DEEPIN_WAYLAND
-void windowStateThread::setupRegistry(Registry *registry)
-{
-    connect(registry, &Registry::compositorAnnounced, this,
-    [this, registry](quint32 name, quint32 version) {
-        m_compositor = registry->createCompositor(name, version, this);
-    }
-           );
-
-    connect(registry, &Registry::clientManagementAnnounced, this,
-    [this, registry](quint32 name, quint32 version) {
-        m_clientManagement = registry->createClientManagement(name, version, this);
-        connect(m_clientManagement, &ClientManagement::windowStatesChanged, this,
-        [this] {
-            m_windowStates = m_clientManagement->getWindowStates();
-            this->waylandwindowinfo(m_windowStates);
-        }
-               );
-    }
-           );
-
-    connect(registry, &Registry::interfacesAnnounced, this,
-    [this] {
-        Q_ASSERT(m_compositor);
-        Q_ASSERT(m_clientManagement);
-        m_windowStates = m_clientManagement->getWindowStates();
-        this->waylandwindowinfo(m_windowStates);
-    }
-           );
-
-    registry->setEventQueue(m_eventQueue);
-    registry->create(m_connectionThreadObject);
-    registry->setup();
-}
-
-void windowStateThread::waylandwindowinfo(const QVector<ClientManagement::WindowState> &windowStates)
-{
-    if (windowStates.count() == 0) {
-        return;
-    }
-
-    bool aheandInCamera = false;
-    QRect rect;
-    for (int i = 0; i < windowStates.count(); ++i) {
-        if (QString(windowStates[i].resourceName) != "deepin-camera") {
-            if (!aheandInCamera)
-                continue;
-            else {
-                if (windowStates[i].isFullScreen) {
-                    QRect fullscreenRect(windowStates[i].geometry.x, windowStates[i].geometry.y,
-                                         windowStates[i].geometry.width, windowStates[i].geometry.height);
-                    if (fullscreenRect.contains(rect))
-                        emit someWindowFullScreen();
-                }
-            }
-        } else {
-            aheandInCamera = true;
-            rect = QRect(windowStates[i].geometry.x, windowStates[i].geometry.y,
-                         windowStates[i].geometry.width, windowStates[i].geometry.height);
-        }
-    }
-}
-#endif
 
