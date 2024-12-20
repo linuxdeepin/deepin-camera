49abf557935e696f516d6b248348394b8092916d
wyu <97944796+wyu71@users.noreply.github.com>
2024-12-20 16:13:37 +0800

    fix: remove invalid  url (#345)
    remove invalid  url
    
    Log: remove invalid  url

--- src/src/circlepushbutton.cpp
+++ src/src/circlepushbutton.cpp
@@ -196,7 +196,7 @@ void circlePushButton::mousePressEvent(QMouseEvent *event)
 
 void circlePushButton::mouseMoveEvent(QMouseEvent *event)
 {
-    //解决bug 在按钮中可拖动相机界面，https://pms.uniontech.com/zentao/bug-view-100647.html
+    //解决bug 在按钮中可拖动相机界面，bug 100647
     Q_UNUSED(event);
     return;
 }
