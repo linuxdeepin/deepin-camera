733fc46b272c20b407a95a5e79a2288d3e553e20
dengzhongyuan <dengzhongyuan@uniontech.com>
2025-08-07 19:55:31 +0800

    chore: Add new Lao translation file
    - Add new Lao translation file
    
    Log: Improve language support by adding Lao translation

---
+++ src/translations/deepin-camera_lo.ts
@@ -0,0 +1,354 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lo">
+<context>
+    <name>CMainWindow</name>
+    <message>
+        <source>Video recording is in progress. Close the window?</source>
+        <translation>ການບັນທຶກວີດີໂອແລະກຳລາການປິດຫນ້າທີ່ກຳລາການບັນທຶກວີດີໂອນີ້ຫຍັງ?</translation>
+    </message>
+    <message>
+        <source>Open folder</source>
+        <translation>ເປີດຟ້ອນ</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>ການຕັ້ງຄ່າ</translation>
+    </message>
+    <message>
+        <source>None</source>
+        <translation>ບໍ່ມີ</translation>
+    </message>
+    <message>
+        <source>Photo</source>
+        <translation>ຮູບພາບ</translation>
+    </message>
+    <message>
+        <source>Video</source>
+        <translation>ວີດີໂອ</translation>
+    </message>
+    <message>
+        <source>Switch Cameras</source>
+        <translation>ປ່ຽນແປງເຄື່ອງສໍາລັບການຖ່າຍ</translation>
+    </message>
+</context>
+<context>
+    <name>CloseDialog</name>
+    <message>
+        <source>Cancel</source>
+        <translation>ຍົກເລີກ</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>ປິດ</translation>
+    </message>
+</context>
+<context>
+    <name>ImageItem</name>
+    <message>
+        <source>Copy</source>
+        <translation>ຄັດລອກ</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>ລົບ</translation>
+    </message>
+    <message>
+        <source>Open folder</source>
+        <translation>ເປີດຟ້ອນ</translation>
+    </message>
+    <message>
+        <source>Print</source>
+        <translation>ພິມ</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>You don&apos;t have permission to operate this folder</source>
+        <translation>ທ່ານບໍ່ມີສິດໃນການດຳເນີນການຟ້ອນນີ້</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>ປິດ</translation>
+    </message>
+    <message>
+        <source>Invalid folder</source>
+        <translation>ບ່ອນຈັດເກັບບໍ່ຖືກຕ້ອງ</translation>
+    </message>
+    <message>
+        <source>Open folder</source>
+        <translation>ເປີດບ່ອນຈັດເກັບ</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>ການຕັ້ງຄ່າ</translation>
+    </message>
+    <message>
+        <source>Basic</source>
+        <translation>ພື້ນຖານ</translation>
+    </message>
+    <message>
+        <source>File location</source>
+        <translation>ທີ່ຕັ້ງຟາຍ</translation>
+    </message>
+    <message>
+        <source>Shoot</source>
+        <translation>ຖ່າຍ</translation>
+    </message>
+    <message>
+        <source>Burst mode</source>
+        <translation>ຮູບແບບການຖ່າຍຕໍ່ເນື່ອງ</translation>
+    </message>
+    <message>
+        <source>Self-timer delay</source>
+        <translation>ເວລາລໍຖ້າຕົວເອງ</translation>
+    </message>
+    <message>
+        <source>Output</source>
+        <translation>ຜົນໄດ້ຮັບ</translation>
+    </message>
+    <message>
+        <source>Output format</source>
+        <translation>ຮູບແບບຜົນໄດ້ຮັບ</translation>
+    </message>
+    <message>
+        <source>4 pcs</source>
+        <translation>4 ອັນ</translation>
+    </message>
+    <message>
+        <source>10 pcs</source>
+        <translation>10 ອັນ</translation>
+    </message>
+    <message>
+        <source>None</source>
+        <translation>ບໍ່ມີ</translation>
+    </message>
+    <message>
+        <source>Camera</source>
+        <translation>ກ້ອງຖ່າຍ</translation>
+    </message>
+    <message>
+        <source>Camera is an image and video capture utility using your PC camera or webcam.</source>
+        <translation>ການຖ່າຍເປັນຕົວຊ່ວຍຈັບຮູບພາບ ແລະ ວິດີໂອໃນການໃຊ້ກ້ອງຖ່າຍຂອງຄອມພິວເຕີ້ຂອງທ່ານ ຫຼື ກ້ອງຖ່າຍວີດີໂອ</translation>
+    </message>
+    <message>
+        <source>File not saved</source>
+        <translation>ຟາຍບໍ່ໄດ້ຖືກບັນທຶກ</translation>
+    </message>
+    <message>
+        <source>Save to:</source>
+        <translation>ບັນທຶກໃສ:</translation>
+    </message>
+    <message>
+        <source>Photo:</source>
+        <translation>ຮູບຖ່າຍ:</translation>
+    </message>
+    <message>
+        <source>Video:</source>
+        <translation>ວີດີໂອ:</translation>
+    </message>
+    <message>
+        <source>Resolution settings</source>
+        <translation>ການຕັ້ງຄ່າຄວາມຊັດເຈນ</translation>
+    </message>
+    <message>
+        <source>Resolution:</source>
+        <translation>ຄວາມຊັດເຈນ:</translation>
+    </message>
+    <message>
+        <source>Photos:</source>
+        <translation>ຮູບຖ່າຍ:</translation>
+    </message>
+    <message>
+        <source>Videos:</source>
+        <translation>ວີດີໂອ:</translation>
+    </message>
+    <message>
+        <source>Sound effects</source>
+        <translation>ສຽງປະກົດການ</translation>
+    </message>
+    <message>
+        <source>Mirror mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Flashlight</source>
+        <translation>ແສງເຄື່ອງຍ່າງ</translation>
+    </message>
+    <message>
+        <source>Gridlines</source>
+        <translation>ເສັ້ນເຕີ່ມ</translation>
+    </message>
+    <message>
+        <source>1*1 lines</source>
+        <translation>ເສັ້ນ 1*1</translation>
+    </message>
+    <message>
+        <source>2*2 lines</source>
+        <translation>ເສັ້ນ 2*2</translation>
+    </message>
+    <message>
+        <source>Maximum resolution:</source>
+        <translation>ຄວາມຊັດເຈນສູງສຸດ:</translation>
+    </message>
+</context>
+<context>
+    <name>Shortcut</name>
+    <message>
+        <source>Camera</source>
+        <translation>ກ້ອງຖ່າຍ</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>ຕັ້ງຄ່າ</translation>
+    </message>
+    <message>
+        <source>Stop recording</source>
+        <translation>ຢຸດບັນທຶກ</translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation>ຄັດລອກ</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>ລົບ</translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation>ເປີດ</translation>
+    </message>
+    <message>
+        <source>Print</source>
+        <translation>ພິມ</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>ຊ່ວຍເຫຼືອ</translation>
+    </message>
+    <message>
+        <source>Display shortcuts</source>
+        <translation>ສະແດງລະຫັດການດຳເນີນການ</translation>
+    </message>
+    <message>
+        <source>Stop taking photos</source>
+        <translation>ຢຸດຖ່າຍຮູບ</translation>
+    </message>
+    <message>
+        <source>Context menu</source>
+        <translation>ລະຫັດການດຳເນີນການ</translation>
+    </message>
+</context>
+<context>
+    <name>ThumbnailsBar</name>
+    <message>
+        <source>Take photo</source>
+        <translation>ຖ່າຍຮູບ</translation>
+    </message>
+    <message>
+        <source>Record video</source>
+        <translation>ບັນທຶກວີດີໂອ</translation>
+    </message>
+    <message>
+        <source>Stop taking photos</source>
+        <translation>ຢຸດຖ່າຍຮູບ</translation>
+    </message>
+</context>
+<context>
+    <name>dc::Settings</name>
+    <message>
+        <source>None</source>
+        <translation>ບໍ່ມີ</translation>
+    </message>
+    <message>
+        <source>mp4</source>
+        <translation>mp4</translation>
+    </message>
+    <message>
+        <source>webm</source>
+        <translation>webm</translation>
+    </message>
+</context>
+<context>
+    <name>filterPreviewButton</name>
+    <message>
+        <source>Normal</source>
+        <translation>ທົ່ວໄປ</translation>
+    </message>
+    <message>
+        <source>Vivid</source>
+        <translation>ສີ່ງເດັ່ນ</translation>
+    </message>
+    <message>
+        <source>Warm</source>
+        <translation>ຮ້ອມ</translation>
+    </message>
+    <message>
+        <source>Cold</source>
+        <translation>ເຢັນ</translation>
+    </message>
+    <message>
+        <source>Retro</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Film</source>
+        <translation>ຟິລມ</translation>
+    </message>
+    <message>
+        <source>Gray</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>B&amp;W</source>
+        <translation>ສີຂາວ-ສີດ່າງ</translation>
+    </message>
+</context>
+<context>
+    <name>takePhotoSettingAreaWidget</name>
+    <message>
+        <source>Flashlight</source>
+        <translation>ແສງສະຫຼາດ</translation>
+    </message>
+    <message>
+        <source>On</source>
+        <translation>ເປີດ</translation>
+    </message>
+    <message>
+        <source>Off</source>
+        <translation>ປິດ</translation>
+    </message>
+    <message>
+        <source>Delay capture</source>
+        <translation>ລໍ້ກ່ອງ</translation>
+    </message>
+    <message>
+        <source>None</source>
+        <translation>ບໍ່ມີ</translation>
+    </message>
+    <message>
+        <source>Filters</source>
+        <translation>ຕາຕະລາງ</translation>
+    </message>
+    <message>
+        <source>Exposure</source>
+        <translation>ການສະແດງສີ</translation>
+    </message>
+</context>
+<context>
+    <name>videowidget</name>
+    <message>
+        <source>No webcam found</source>
+        <translation>ບໍ່ພົບກ້ອງຖ່າຍວີດີໂອ</translation>
+    </message>
+    <message>
+        <source>The webcam is in use</source>
+        <translation>ກ້ອງຖ່າຍວີດີໂອຖືກໃຊ້ງານ</translation>
+    </message>
+    <message>
+        <source>Stop recording</source>
+        <translation>ຢຸດບັນທຶກ</translation>
+    </message>
+</context>
+</TS>
