a87b7027765e9e0d1061cd2230148f800f430206
lichaofan <lichaofan@uniontech.com>
2025-10-24 11:32:28 +0800

    chore: Correction of some misspelled words.
    一些单词拼写错误的纠正。
    phote => photo
    nublock => unblock
    offect => offset
    priview => preview
    pictrue => picture
    falsh => flash
    
    v20 BUG 分支合一到v25主线
    Task: https://pms.uniontech.com/task-view-383481.html

--- src/src/videowidget.cpp
+++ src/src/videowidget.cpp
@@ -40,9 +40,9 @@
 
 #define COUNTDOWN_WIDTH 32
 #define COUNTDOWN_HEIGHT 61
-#define COUNTDOWN_OFFECT 20
+#define COUNTDOWN_OFFSET 20
 
-static PRIVIEW_ENUM_STATE g_Enum_Camera_State = PICTRUE;
+static PREVIEW_ENUM_STATE g_Enum_Camera_State = PICTURE;
 
 // 重写QGraphicsView类，在子类中，直接将鼠标事件路由到QWidget，这样QGraphicsScene不会接收和处理鼠标事件
 // bug链接 bug 100791
@@ -581,12 +581,12 @@ void videowidget::onReachMaxDelayedFrames()
     qDebug() << "Camera stopped due to maximum delayed frames";
 }
 
-void videowidget::showCountDownLabel(PRIVIEW_ENUM_STATE state)
+void videowidget::showCountDownLabel(PREVIEW_ENUM_STATE state)
 {
     // qDebug() << "Entering showCountDownLabel";
     QString str;
     switch (state) {
-    case PICTRUE:
+    case PICTURE:
         // qDebug() << "Showing countdown label for photo";
         m_recordingTimeWidget->hide();
         break;
@@ -665,7 +665,7 @@ void videowidget::resizeEvent(QResizeEvent *size)
                                 height() - m_recordingTimeWidget->height() - 15);
 
     m_dLabel->move((width() - m_dLabel->width()) / 2,
-                   (height() - m_dLabel->height()) - COUNTDOWN_OFFECT);
+                   (height() - m_dLabel->height()) - COUNTDOWN_OFFSET);
 
 
     if (m_pCamErrItem->isVisible()) {
@@ -727,7 +727,7 @@ void videowidget::showCountdown()
             emit updateRecordState(photoRecordBtn::Recording);
         }
 
-        if (g_Enum_Camera_State == PICTRUE) {
+        if (g_Enum_Camera_State == PICTURE) {
             qDebug() << "Showing countdown label for photo";
             if (m_nInterval == 0 && m_curTakePicTime >= 0) {
                 if (m_flashEnable && 1 == m_nMaxContinuous) {
@@ -839,7 +839,7 @@ void videowidget::showCountdown()
         qDebug() << "Showing countdown label for photo";
         m_dLabel->setText(QString::number(m_nInterval));
         m_dLabel->show();
-        showCountDownLabel(PICTRUE); //拍照录像都要显示倒计时
+        showCountDownLabel(PICTURE); //拍照录像都要显示倒计时
         m_nInterval--;
         qInfo() << "m_nInterval:" << m_nInterval;
     }
@@ -1274,7 +1274,7 @@ QString videowidget::getSaveVdFolder() const
 void videowidget::onTakePic(bool bTrue)
 {
     qDebug() << "Take photo triggered:" << bTrue;
-    g_Enum_Camera_State = PICTRUE;
+    g_Enum_Camera_State = PICTURE;
 
     if (bTrue) {
         qDebug() << "Taking photo";
