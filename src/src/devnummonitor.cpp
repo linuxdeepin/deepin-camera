ba6e577b1abae348f0c0d88ed99fb5b9a9342d60
lichaofan <lichaofan@uniontech.com>
2025-11-04 19:41:28 +0800

    fix: When the screen is locked while using the camera, face recognition fails to work after unlocking.
    The camera occupies the camera device, preventing the unlocking program from accessing it. Therefore, the camera should release the camera device when the screen is locked and re-occupy it after unlocking.
    
    fix: 使用相机的时候锁屏，解锁无法使用人脸识别。
    
    相机占用了摄像头设备，导致解锁程序打不开摄像头设备。所以，相机应该在锁屏的时候释放摄像头设备，待解锁之后再占用摄像头设备。
    
    Bug: https://pms.uniontech.com/bug-view-339625.html
    
    v20 BUG 分支合一到v25主线
    Task: https://pms.uniontech.com/task-view-383481.html

--- src/src/devnummonitor.cpp
+++ src/src/devnummonitor.cpp
@@ -62,7 +62,10 @@ void DevNumMonitor::timeOutSlot()
             }
         } else {
             if (m_noDevice) {
-                qWarning() << "Device found";
+                v4l2_dev_sys_data_t *v4l2_devices = get_device_list()->list_devices;
+                if (v4l2_devices) {
+                    qWarning() << "Device found:" << v4l2_devices[0].device << v4l2_devices[0].name;
+                }
             }
             m_noDevice = false;
             emit existDevice();
